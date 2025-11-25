6990d6c316303b35f5f48343e44477cde6d4a334
knokko <tim@codeyellow.nl>
2025-11-25 17:56:03 +0100

    slightly more doc comments

--- content/src/main/kotlin/mardek/content/battle/PotentialItem.kt
+++ content/src/main/kotlin/mardek/content/battle/PotentialItem.kt
@@ -25,6 +25,9 @@ class PotentialItem(
 	@ReferenceField(stable = false, label = "items")
 	val item: Item?,
 
+	/**
+	 * The chance to get `item`, in percentages
+	 */
 	@BitField(id = 1)
 	@IntegerField(expectUniform = true, minValue = 1, maxValue = 100)
 	val chance: Int,
@@ -39,9 +42,15 @@ class PotentialItem(
 	override fun toString() = "$chance% $item"
 }
 
+/**
+ * The potential equipment that a `Monster` can have in a given slot.
+ */
 @BitStruct(backwardCompatible = true)
 class PotentialEquipment(
 
+	/**
+	 * The potential items that the `Monster` can get. The sum of their `chance` must be 100%.
+	 */
 	@BitField(id = 0)
 	val entries: ArrayList<PotentialItem>
 ) {
@@ -58,6 +67,10 @@ class PotentialEquipment(
 
 	override fun toString() = "Equipment($entries)"
 
+	/**
+	 * (Randomly) picks an item from this `PotentialEquipment`, using the `chance`s of each `PotentialItem` in
+	 * `entries`.
+	 */
 	fun pick(): Item? {
 		val total = entries.sumOf { it.chance }
 		val selected = Random.nextInt(total)
@@ -72,16 +85,32 @@ class PotentialEquipment(
 	}
 
 	companion object {
+
+		/**
+		 * A `PotentialEquipment` whose `pick()` method will always return `null`. When a monster has this
+		 * `PotentialEquipment` for e.g. its shield equipment slot, the monster will never carry a shield.
+		 */
 		val EMPTY = PotentialEquipment(arrayListOf(PotentialItem(null, 100)))
 	}
 }
 
+/**
+ * Represents a `PlotItem` that a monster may drop after it is slain.
+ */
 @BitStruct(backwardCompatible = true)
 class PotentialPlotItem(
+
+	/**
+	 * The `PlotItem` that may be dropped.
+	 */
 	@BitField(id = 0)
 	@ReferenceField(stable = false, label = "plot items")
 	val item: PlotItem,
 
+	/**
+	 * The chance that `item` is dropped. This is usually 100% chance, but there are exceptions (e.g. the zombies in
+	 * Canonia may or may not drop a Trilobite Key).
+	 */
 	@BitField(id = 1)
 	@IntegerField(expectUniform = false, minValue = 0, maxValue = 100)
 	val chance: Int,
--- content/src/main/kotlin/mardek/content/characters/PlayableCharacter.kt
+++ content/src/main/kotlin/mardek/content/characters/PlayableCharacter.kt
@@ -14,35 +14,72 @@ import mardek.content.stats.CreatureType
 import java.util.*
 import kotlin.collections.ArrayList
 
+/**
+ * Represents a character that can be controlled by the player, e.g. Hero Mardek and Deugan.
+ */
 @BitStruct(backwardCompatible = true)
 class PlayableCharacter(
+
+	/**
+	 * The (display) name of the character. Note that this is not necessarily unique. For instance, there are 3
+	 * `PlayableCharacter`s whose `name` is "Deugan": Hero Deugan, child Deugan, and soldier Deugan. Use
+	 * `characterClass.rawName` for a more... unique... name.
+	 */
 	@BitField(id = 0)
 	val name: String,
 
+	/**
+	 * The 'class' of the playable character. This determines which weapons/armor the character can carry, and which
+	 * action skills it can use. As a bonus, it also provides a unique name for the character.
+	 */
 	@BitField(id = 1)
 	@ReferenceField(stable = false, label = "character classes")
 	val characterClass: CharacterClass,
 
+	/**
+	 * The element of the playable character (e.g. `Light` for Mardek).
+	 */
 	@BitField(id = 2)
 	@ReferenceField(stable = false, label = "elements")
 	val element: Element,
 
+	/**
+	 * The base stats of the character. This list should have an entry for `Strength`, `Spirit`, `Vitality`, and
+	 * `Agility`.
+	 */
 	@BitField(id = 3)
 	val baseStats: ArrayList<StatModifier>,
 
+	/**
+	 * The sprites that will be rendered when this character is walking around in an area.
+	 */
 	@BitField(id = 4)
 	val areaSprites: DirectionalSprites,
 
+	/**
+	 * The animations that will be rendered when this character is in combat. Note that one of its animations will be
+	 * rendered at any point in time during a battle.
+	 */
 	@BitField(id = 5)
 	val animations: CombatantAnimations,
 
+	/**
+	 * The creature type of the playable character, which is almost always HUMAN.
+	 */
 	@BitField(id = 6)
 	val creatureType: CreatureType,
 
+	/**
+	 * The portrait of the playable character, which is used in dialogue, in the party tab, and in battle (when the
+	 * character is on turn).
+	 */
 	@BitField(id = 7)
 	@ReferenceField(stable = false, label = "portrait info")
 	val portraitInfo: PortraitInfo,
 
+	/**
+	 * The unique ID of the playable character, which is used for (de)serialization.
+	 */
 	@BitField(id = 8)
 	@StableReferenceFieldId
 	val id: UUID,
