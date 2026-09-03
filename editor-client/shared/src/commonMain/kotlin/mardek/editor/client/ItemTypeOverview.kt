65bf715bf5a3daa53b44add68cae6e0c8bde2dcb
knokko <tim@codeyellow.nl>
2026-09-03 15:02:59 +0200

    first table

--- editor-client/shared/src/commonMain/kotlin/mardek/editor/client/ItemTypeOverview.kt
+++ editor-client/shared/src/commonMain/kotlin/mardek/editor/client/ItemTypeOverview.kt
@@ -1,8 +1,19 @@
 package mardek.editor.client
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.github.knokko.boiler.utilities.ColorPacker.rgb
 import mardek.content.inventory.ItemType
 
@@ -12,18 +23,25 @@ val itemTypes = mutableListOf(
 	ItemType("HELMET: FULL", rgb(0, 0, 200), "Full Helmet"),
 )
 
+val baseModifiers = arrayOf(300.dp, 300.dp, 300.dp).map { Modifier.width(it) }
+
 @Composable
 fun ItemTypeOverview() {
-	Row {
-		Text("Raw name")
-		Text("Grid color")
-		Text("Nice name")
-	}
-	for (itemType in itemTypes) {
-		Row {
-			Text(itemType.displayName)
-			Text(itemType.gridColor.toString())
-			Text(itemType.niceName)
+	val scrollState = rememberScrollState()
+	Column(modifier = Modifier.fillMaxSize()) {
+		Row(modifier = Modifier.background(Color.Yellow).horizontalScroll(scrollState)) {
+			Text("Upper name", modifier = baseModifiers[0])
+			Text("Nice name", modifier = baseModifiers[1])
+			Text("Grid color", modifier = baseModifiers[2])
+		}
+		LazyColumn(modifier = Modifier.fillMaxSize().background(Color.Cyan)) {
+			items(itemTypes) { itemType ->
+				Row(modifier = Modifier.horizontalScroll(scrollState)) {
+					Text(itemType.displayName, modifier = baseModifiers[0])
+					Text(itemType.gridColor.toString(), modifier = baseModifiers[1])
+					Text(itemType.niceName, modifier = baseModifiers[2])
+				}
+			}
 		}
 	}
 }
